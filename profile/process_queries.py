5af4c9e4a2a173c35786654416df1d921656d7c4
xhOwenMa <m.owen@wustl.edu>
2023-12-03 14:39:10 -0600

    update voice

--- profile/process_queries.py
+++ profile/process_queries.py
@@ -1,10 +1,10 @@
 # Open the file and read the contents
-with open('/Users/owenma/Desktop/527final/voice/voice_prompts.txt', 'r') as f:
+with open('/Users/owenma/Desktop/527final/voice/tina/voice_prompts.txt', 'r') as f:
     lines = f.readlines()
 
 # Process each line to remove quotation marks
 processed_lines = [line.replace('"', '') for line in lines]
 
 # Write the processed contents back to the file
-with open('/Users/owenma/Desktop/527final/voice/voice_prompts.txt', 'w') as f:
+with open('/Users/owenma/Desktop/527final/voice/tina/voice_prompts.txt', 'w') as f:
     f.writelines(processed_lines)
